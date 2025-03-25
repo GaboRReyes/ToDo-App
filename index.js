3f70de25bb681c9e9a03403d61e60d255b0de0a4
GaboRReyes <160171479+GaboRReyes@users.noreply.github.com>
2025-03-25 13:34:06 -0600

    Titulo y Descripcion
    Se añadio una nueva funcionalidad al boton add, obtiene de nuestra pagina el titulo y la descripcion de la nueva tarea

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const btn= document.getElementById('add');
-btn.addEventListener('click',function(){
-    console.log('Click');
+document.addEventListener('DOMContentLoaded',function(){
+    const title= document.getElementById('title');
+    const description= document.getElementById('description');
+    const btn= document.getElementById('add');
+    const table= document.getElementById('table');
+
+    btn.onclick = addTodo;
+
+    function addTodo(){
+       if (title.value === '' || description.value ===''){
+        console.error('Title and description are recquired')
+       } else{
+        console.log('Ok')
+       }
+    }
 })
