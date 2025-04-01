57c622c835c4b554b8bc1ea9ae36fedb866ec09c
Torrecillas Pablo <22030092@itcelaya.edu.mx>
2025-03-31 23:31:40 -0600

    error de sintaxis corregido

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded',function(){
     const title= document.getElementById('title');
     const description= document.getElementById('description');
+    const table= document.getElementById('table');
     const alert = document.getElementById('alert');
     const btn= document.getElementById('add');
-    const table= document.getElementById('table');
     let id=1;
-
-    btn.onclick = addTodo;
 
     function removeTodo(id){
         document.getElementById(id).remove();
@@ -14,12 +12,14 @@ document.addEventListener('DOMContentLoaded',function(){
 
     function addTodo(){
        if (title.value === '' || description.value ===''){
+        alert.classList.remove('d-none');
         alert.innerText = 'Title and description are required';
         return;
     }
-    alert.classList.remove('d-none');
+
+    alert.classList.add('d-none');
     const row = table.insertRow();
-    row.setAtrribute('id', id++);
+    row.setAttribute('id', id++);
     row.innerHTML = `
     <td>${title.value}</td>
     <td>${description.value}</td>
@@ -33,12 +33,12 @@ document.addEventListener('DOMContentLoaded',function(){
     </td>
     `;
     const removeBtn = document.createElement('button');
-    removeBtn.classList.add('btn', 'btn-danger');
+    removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'ml-1');
     removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
     removeBtn.onclick = function (){
         removeTodo(row.getAttribute('id'));
     }
     row.children[3].appendChild(removeBtn);
 }
-
+btn.onclick = addTodo;
 });
